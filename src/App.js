6e12e8a1826363b266c0e75511c4b59bf5038002
yj1995 <33519621+yj1995@users.noreply.github.com>
2021-04-11 16:55:30 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from 'react';
+import styles from './styles/index.scss'
+import { Data } from './constant/data'
+
+export function App() {
+  const [displayData, setDisplayData] = useState([])
+  const [orderingData, setOrderingData] = useState([])
+  let [orderValue, setOrderValue] = useState('Asc')
+  let [search, setSearchValue] = useState('')
+
+  useEffect(() => {
+    const sortedData = Data.sort((a, b) => a.id - b.id);
+    setDisplayData(sortedData)
+    setOrderingData(sortedData)
+  }, [])
+
+  const renderValue = ({ id, name }) => (
+    <div key={id + name}>
+      <span className={styles.id}>{id}</span>
+      <span className={styles.name}>{name}</span>
+    </div>
+  )
+
+  const renderHeader = () => (
+    <div>
+      <span className={styles.id}>ID</span>
+      <span className={styles.name}>Name</span>
+    </div>
+  )
+
+  const changeRadioValue = (e) => {
+    const { value } = e.target
+    setOrderValue(value)
+    setDisplayData([...displayData].reverse())
+    setOrderingData([...displayData].reverse())
+  }
+
+  const searchValue = (e) => {
+    const { value } = e.target
+    setSearchValue(value)
+    const filterData = orderingData.filter((data) => (data.name.toLowerCase().match(value.toLowerCase())))
+    setDisplayData(filterData)
+  }
+
+  const renderFilter = () => (
+    <div className={styles.filterParent}>
+      <label className={styles.radioSpace}><input type="radio" value="Asc" name="order" checked={orderValue === "Asc"} onChange={changeRadioValue} /> Asc</label>
+      <label className={styles.radioSpace}><input type="radio" value="Des" name="order" checked={orderValue === "Des"} onChange={changeRadioValue} /> Des</label>
+      <input className={styles.searchFields} value={search} placeholder='Search Name' onChange={searchValue} />
+    </div>
+  )
+
+  return (
+    <div className={styles.mainWrapper}>
+      <div className={styles.allParent}>
+        <div className={styles.header}>Application Data</div>
+        <div className={styles.filter}>
+          {renderFilter()}
+        </div>
+        <div className={styles.tableData}>
+          {displayData.length ? (
+            <>
+              {renderHeader()}
+              {displayData.map((val) => (renderValue(val)))}
+            </>) : 'No Data Available'}
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/constant/data.js
@@ -0,0 +1,82 @@
+export const Data = [
+    {
+        id: 1,
+        name: 'aa'
+    },
+    {
+        id: 5,
+        name: 'ab'
+    },
+    {
+        id: 4,
+        name: 'ca'
+    },
+    {
+        id: 2,
+        name: 'ff'
+    },
+    {
+        id: 3,
+        name: 'gg'
+    },
+    {
+        id: 10,
+        name: 'hh'
+    },
+    {
+        id: 9,
+        name: 'cc'
+    },
+    {
+        id: 6,
+        name: 'qq'
+    },
+    {
+        id: 8,
+        name: 'zz'
+    },
+    {
+        id: 7,
+        name: 'pp'
+    },
+    {
+        id: 15,
+        name: 'ww'
+    },
+    {
+        id: 13,
+        name: 'll'
+    },
+    {
+        id: 12,
+        name: 'yy'
+    },
+    {
+        id: 14,
+        name: 'tt'
+    },
+    {
+        id: 11,
+        name: 'rr'
+    },
+    {
+        id: 19,
+        name: 'xc'
+    },
+    {
+        id: 17,
+        name: 'ap'
+    },
+    {
+        id: 16,
+        name: 'ad'
+    },
+    {
+        id: 18,
+        name: 'ah'
+    },
+    {
+        id: 20,
+        name: 'mm'
+    }
+]
